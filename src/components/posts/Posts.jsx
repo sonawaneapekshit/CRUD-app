0e10ffbe45a84266053a793485ee55dd9dee814d
sonawaneapekshit <apekshitsonawane@gmail.com>
2025-10-21 13:33:45 +0530

    search posts logic updates

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,25 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Loader } from '../loader/Loader';
 import Card from '../card/Card';
 import PropTypes from 'prop-types';
 
-const Posts = ({ postArray, loading }) => {
-useEffect(()=> {
+const Posts = ({ postArray, loading, filterText }) => {
+  const [filteredPosts, setFilteredPosts] = useState([postArray]);
+  // const filterPostsList = useRef(postArray);
 
-}, filterText)
   console.log(postArray);
   return (
     <div className="min-h-80 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 auto-rows-fr">
       {loading ? (
         <Loader />
       ) : postArray.length > 0 ? (
-        postArray.map((post) => (
-          <Card
-            sizeClasses="h-full"
-            key={post.id}
-            post={post}
-          />
-        ))
+        postArray
+          .filter((post) =>
+            filterText !== ''
+              ? typeof filterText === 'number'
+                ? post.id == filterText
+                : post.title.includes(filterText)
+              : true,
+          )
+          .map((post) => (
+            <Card sizeClasses="h-full" key={post.id} post={post} />
+          ))
       ) : (
         <div>No data present</div>
       )}
@@ -30,7 +34,7 @@ useEffect(()=> {
 Posts.propTypes = {
   postArray: PropTypes.array,
   loading: PropTypes.bool,
-  filterText: PropTypes.string
+  filterText: PropTypes.string,
 };
 
 export default Posts;
