2219fa8c8d33c3217d223c97322db97ae41e711c
sonawaneapekshit <apekshitsonawane@gmail.com>
2025-10-26 20:53:25 +0530

    filtered posts fix added for no results after filter

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -4,28 +4,39 @@ import Card from '../card/Card';
 import PropTypes from 'prop-types';
 
 const Posts = ({ postArray, loading, filterText }) => {
-  const [filteredPosts, setFilteredPosts] = useState([postArray]);
+  // const [filteredPosts, setFilteredPosts] = useState([postArray]);
   // const filterPostsList = useRef(postArray);
+
+  const getFilteredPosts = () => {
+    if (filterText === '') return postArray;
+    return postArray.filter((post) =>
+      typeof filterText === 'number'
+        ? post.id === filterText
+        : post.title.includes(filterText),
+    );
+  };
+
+  let filteredPosts = getFilteredPosts();
 
   console.log(postArray);
   return (
-    <div className="min-h-80 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 auto-rows-fr">
+    <div
+      className={`min-h-80 grid ${
+        filteredPosts.length > 0
+          ? 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'
+          : 'grid-cols-1'
+      } gap-4 auto-rows-fr`}
+    >
       {loading ? (
         <Loader />
-      ) : postArray.length > 0 ? (
-        postArray
-          .filter((post) =>
-            filterText !== ''
-              ? typeof filterText === 'number'
-                ? post.id === filterText
-                : post.title.includes(filterText)
-              : true,
-          )
-          .map((post) => (
-            <Card sizeClasses="h-full" key={post.id} post={post} />
-          ))
+      ) : filteredPosts.length > 0 ? (
+        filteredPosts.map((post) => (
+          <Card sizeClasses="h-full" key={post.id} post={post} />
+        ))
       ) : (
-        <div>No data present</div>
+        <div className="h-full w-full text-center text-2xl font-bold flex justify-center items-center">
+          No data present
+        </div>
       )}
     </div>
   );
